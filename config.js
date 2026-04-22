3681c95ec74d5e4a4f85fdf9e37b2b3821fa6a7b
1prestigegsd-555 <1prestige.gsd@gmail.com>
2026-04-22 15:29:05 +0200

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@
 // ╚══════════════════════════════════════════════════════════════════════════╝
 
 window.APP_CONFIG = {
-  const SUPABASE_URL      = 'https://exeaousazcuwogvdpvqn.supabase.co'
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV4ZWFvdXNhemN1d29ndmRwdnFuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY4MzU3OTQsImV4cCI6MjA5MjQxMTc5NH0.az5w1btugx0iaJiV2iJ6zTsZ6A_aLBB3grun89hx35U'
-const PASSWORD_HASH     = '340438f651ed9852a56f87f4a6881ede6bb1989b0e420911878b2823eeb5a2d7',
+SUPABASE_URL:'https://exeaousazcuwogvdpvqn.supabase.co'
+SUPABASE_ANON_KEY:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV4ZWFvdXNhemN1d29ndmRwdnFuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY4MzU3OTQsImV4cCI6MjA5MjQxMTc5NH0.az5w1btugx0iaJiV2iJ6zTsZ6A_aLBB3grun89hx35U'
+PASSWORD_HASH:'340438f651ed9852a56f87f4a6881ede6bb1989b0e420911878b2823eeb5a2d7',
 }
